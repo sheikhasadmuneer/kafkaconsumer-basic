05c07c8a6551c2c36b6f9128d772ff28f7575803
Asad Muneer <sheikhasadmuneer@hotmail.com>
2024-09-22 14:43:07 +0200

    kafka consumer basic

---
+++ src/main/java/com/example/user_app_kafka/UserAppKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.example.user_app_kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAppKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserAppKafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/user_app_kafka/UserAppKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.user_app_kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAppKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
